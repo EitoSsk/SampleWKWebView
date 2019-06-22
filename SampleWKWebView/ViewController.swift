a9eb0b5f72b8232ecab7129e3c8063aba2c5361f
EitoSsk <bulletman813@gmail.com>
2019-06-22 20:36:25 +0900

    Initial Commit

---
+++ SampleWKWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleWKWebView
+//
+//  Created by beams001 on 2019/06/22.
+//  Copyright © 2019 bms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
